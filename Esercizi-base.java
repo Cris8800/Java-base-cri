e90f2f2765f2026f1ff8ddd39d168c71855a1736
Cris8800 <cristinaperez@hotmail.it>
2022-11-21 15:54:29 +0100

    primo

---
+++ Esercizi-base.java
@@ -0,0 +1,22 @@
+/*public class Hello {
+     public static void main(String[] args) { 
+
+        System.out.println("ciao"+ valore);
+         int valore = 3;
+    }
+}*/
+
+//Esercizio 1
+
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("+ - - +");
+         System.out.println("| Cri |");
+          System.out.println("+ - - +");
+    }
+}
+
+
+
+//Esercizio 2 
