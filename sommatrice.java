21544a4570b84b416c9177502f8d65fb758372e4
Cris8800 <cristinaperez@hotmail.it>
2022-11-22 11:25:51 +0100

    esercizi

---
+++ sommatrice.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class sommatrice {
+    public static void main(String... args) { 
+        Scanner input = new Scanner(System.in);
+            System.out.println("Inserisci un numero....");
+             int n1 = input.nextInt ();
+   System.out.println("Inserisci un altro numero....");
+             int n2 = input.nextInt();
+            System.out.println("La somma è " + (n1 + n2));
+        }
+
+    }
+
+
